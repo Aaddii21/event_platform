1bfe231d3f8b6fd2e744d2a6ba0bb41f85eec63d
Aaddii21 <aaddiiguleria24@gmail.com>
2024-04-27 20:46:55 +0530

    Update Footer.tsx

--- components/shared/Footer.tsx
+++ components/shared/Footer.tsx
@@ -3,19 +3,20 @@ import Link from "next/link"
 
 const Footer = () => {
   return (
-  <footer className="border-t">
-    <div className="flex-center Wrapper flex-between flex flex-col gap--4 p-5 text-center sm:flex-row">
-      <Link href='/' >
-        <Image
-           src="/assets/images/logo.svg"
-           alt="logo"
-           width={128}
-           height={38}
-           />
-     </Link>
-     <p>2024 Eventify. All Rights Reserved</p>
-    </div>
-  </footer>
+    <footer className="border-t">
+      <div className="flex-center wrapper flex-between flex flex-col gap-4 p-5 text-center sm:flex-row">
+        <Link href='/'>
+          <Image 
+            src="/assets/images/logo.svg"
+            alt="logo"
+            width={128}
+            height={38}
+          />
+        </Link>
+
+        <p>2023 Evently. All Rights reserved.</p>
+      </div>
+    </footer>
   )
 }
 
