58930c1ebefa11a157c187f2f188fc13840baf77
Aaddii21 <aaddiiguleria24@gmail.com>
2024-05-02 03:42:06 +0530

    first commit

--- components/shared/Header.tsx
+++ components/shared/Header.tsx
@@ -2,7 +2,7 @@ import { SignedIn, SignedOut, UserButton } from "@clerk/nextjs"
 import Image from "next/image"
 import Link from "next/link"
 import { Button } from "../ui/button"
-import NavItems from "./NavItems"
+
 import MobileNav from "./MobileNav"
 
 const Header = () => {
@@ -15,12 +15,6 @@ const Header = () => {
             alt="Evently logo" 
           />
         </Link>
-
-        <SignedIn>
-          <nav className="md:flex-between hidden w-full max-w-xs">
-            <NavItems />
-          </nav>
-        </SignedIn>
 
         <div className="flex w-32 justify-end gap-3">
           <SignedIn>
