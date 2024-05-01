38b0c20640b9105401e2cf295f34b0d6ba3760e5
Aaddii21 <aaddiiguleria24@gmail.com>
2024-05-01 14:01:26 +0530

    Update FileUploader.tsx

--- components/shared/FileUploader.tsx
+++ components/shared/FileUploader.tsx
@@ -1,7 +1,7 @@
 'use client'
 
 import { useCallback, Dispatch, SetStateAction } from 'react'
-import type { FileWithPath } from '@uploadthing/react'
+
 import { useDropzone } from '@uploadthing/react/hooks'
 import { generateClientDropzoneAccept } from 'uploadthing/client'
 
@@ -14,11 +14,7 @@ type FileUploaderProps = {
   setFiles: Dispatch<SetStateAction<File[]>>
 }
 
-export function FileUploader({ imageUrl, onFieldChange, setFiles }: FileUploaderProps) {
-  const onDrop = useCallback((acceptedFiles: FileWithPath[]) => {
-    setFiles(acceptedFiles)
-    onFieldChange(convertFileToUrl(acceptedFiles[0]))
-  }, [])
+
 
   const { getRootProps, getInputProps } = useDropzone({
     onDrop,
