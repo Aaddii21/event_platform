d8bf3e080e60e035fdab900041d945775d427824
Aaddii21 <aaddiiguleria24@gmail.com>
2024-05-01 12:09:00 +0530

    implement stripe

--- components/shared/Header.tsx
+++ components/shared/Header.tsx
@@ -1,10 +1,9 @@
-import {  SignedIn, SignedOut, UserButton,  } from "@clerk/nextjs"
+import { SignedIn, SignedOut, UserButton } from "@clerk/nextjs"
 import Image from "next/image"
 import Link from "next/link"
 import { Button } from "../ui/button"
 import NavItems from "./NavItems"
 import MobileNav from "./MobileNav"
-
 
 const Header = () => {
   return (
@@ -16,18 +15,18 @@ const Header = () => {
             alt="Evently logo" 
           />
         </Link>
-       <SignedIn>
-        <nav className="md:flex-between hidden w-full max-w-xs">
-          <NavItems />
-        </nav>
-       </SignedIn>
+
+        <SignedIn>
+          <nav className="md:flex-between hidden w-full max-w-xs">
+            <NavItems />
+          </nav>
+        </SignedIn>
 
         <div className="flex w-32 justify-end gap-3">
           <SignedIn>
-            <UserButton afterSignOutUrl="/"/>
+            <UserButton afterSignOutUrl="/" />
             <MobileNav />
           </SignedIn>
-         
           <SignedOut>
             <Button asChild className="rounded-full" size="lg">
               <Link href="/sign-in">
