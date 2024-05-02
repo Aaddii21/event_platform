7af21e31871abc4ecd84e541adb521284f8bd55b
Aaddii21 <aaddiiguleria24@gmail.com>
2024-05-02 10:21:41 +0530

    first commit

--- components/shared/Header.tsx
+++ components/shared/Header.tsx
@@ -2,7 +2,7 @@ import { SignedIn, SignedOut, UserButton } from "@clerk/nextjs"
 import Image from "next/image"
 import Link from "next/link"
 import { Button } from "../ui/button"
-
+import NavItems from "./NavItems"
 import MobileNav from "./MobileNav"
 
 const Header = () => {
@@ -12,9 +12,15 @@ const Header = () => {
         <Link href="/" className="w-36">
           <Image 
             src="/assets/images/logo.svg" width={128} height={38}
-            alt="Eventify logo" 
+            alt="Evently logo" 
           />
         </Link>
+
+        <SignedIn>
+          <nav className="md:flex space-x-6 hidden w-full max-w-xs">
+            <NavItems />
+          </nav>
+        </SignedIn>
 
         <div className="flex w-32 justify-end gap-3">
           <SignedIn>
